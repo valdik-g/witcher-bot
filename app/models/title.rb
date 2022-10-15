449af46d6935ab54cb0b9a389582ffae901000d1
valdik-g <galuxa2002@mail.ru>
2022-10-15 09:00:21 +0300

    fix issues

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,3 +1,8 @@
 class Title < ApplicationRecord
   has_and_belongs_to_many :passports
+  before_destroy :update_passporst
+
+  def update_passporst
+    passports.each{ |passport| passport.update!(main_title_id: nil) }
+  end
 end
