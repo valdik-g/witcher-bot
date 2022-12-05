9469bf60fc873e0821327620c30b7b7ce0c36092
uhalukha <UHalukha@ibagroup.eu>
2022-12-05 19:59:46 +0300

    add message for notification

--- app/jobs/birthday_job.rb
+++ app/jobs/birthday_job.rb
@@ -5,6 +5,13 @@ class BirthdayJob < ApplicationJob
 
   def perform()
     token = "5587814730:AAFci39iNXTgIeDLVTvKpCjULW2a94zbuP8"
+
+    send_m_kb = [
+      Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Написать Анри', url: 'https://t.me/andreiperevoshchikov')
+    ]
+    
+    send_markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: send_m_kb)
+
     Telegram::Bot::Client.run(token) do |bot|
       Passport.all.each do |passport|
         unless passport.bd.blank?
@@ -12,10 +19,9 @@ class BirthdayJob < ApplicationJob
           if "#{passport.bd.split('.')[0]}.#{passport.bd.split('.')[1]}" == day
             passports_to_send = Passport.all - [passport]
             passports_to_send.each do |pass|
-              p "i am sending message"
               bot.api.send_message(chat_id: User.find_by(:passport_id => pass.id).telegram_id,
               text: "\xF0\x9F\x8E\x8A Через 3 дня день рождения у #{passport.nickname} \xF0\x9F\x8E\x8A
-              Не забудь поздравить)")
+              Если хочешь порадовать именниника подарком пиши Анри", reply_markup: send_m_kb)
             end
           end
         end
